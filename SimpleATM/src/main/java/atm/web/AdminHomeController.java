04cca1ac37612496ad0afa3cc890a359262e3447
Williaz <williaz257@yahoo.com>
2016-08-01 21:59:13 -0400

    Done delete Foreign key->del func->hibernate many to one

--- SimpleATM/src/main/java/atm/web/AdminHomeController.java
+++ SimpleATM/src/main/java/atm/web/AdminHomeController.java
@@ -1,5 +1,8 @@
 package atm.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import atm.bean.Customer;
 import atm.service.AdminService;
@@ -52,6 +56,38 @@ public class AdminHomeController {
 	  return "redirect:/adminHome";
 	}
 	
+	@RequestMapping(value="/all",method=RequestMethod.GET)
+	public String viewAll(RedirectAttributes model) {
+	  List<Customer> lists= adminService.viewAccounts();
+	  model.addFlashAttribute("reports", lists);
+	  
+	  return  "redirect:/adminHome";
+	  
+	}
+	
+	@RequestMapping(value="/location",method=RequestMethod.GET)
+    public String searchByLocation(@RequestParam String permAddress,
+                                   RedirectAttributes model) {
+      List<Customer> lists= adminService.viewAccountsByLocation(permAddress);
+      model.addFlashAttribute("reports", lists);
+      
+      return  "redirect:/adminHome";
+      
+    }
+	
+	@RequestMapping(value="/one",method=RequestMethod.GET)
+    public String viewOne(@RequestParam Long id,
+                          RedirectAttributes model) {
+      List<Customer> lists= new ArrayList();
+      lists.add(adminService.viewCustomerDetails(id));
+      model.addFlashAttribute("reports", lists);
+      
+      return  "redirect:/adminHome";
+      
+    }
+	
+	
+	///GK
 	@RequestMapping(value="/delete",method=RequestMethod.GET)
     public String delete(@RequestParam Long id, Model model) {
 	  boolean flag=adminService.deleteAccount(id);
